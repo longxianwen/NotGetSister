19aab010e7270f653ed180d94113ee73ef5ad12e
longxw1130 <longxw1130@126.com>
2015-09-21 00:37:24 +0800

    保存图片...

--- 百思不得姐/百思不得姐/Classes/Essence-精华/Controller/XWSeeBigPictureViewController.m
+++ 百思不得姐/百思不得姐/Classes/Essence-精华/Controller/XWSeeBigPictureViewController.m
@@ -9,15 +9,30 @@
 #import "XWSeeBigPictureViewController.h"
 #import "XWTopic.h"
 #import <SDWebImage/UIImageView+WebCache.h>
+#import <SVProgressHUD/SVProgressHUD.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface XWSeeBigPictureViewController ()<UIScrollViewDelegate>
 
 /** 图片 */
 @property (nonatomic, weak) UIImageView *imageView;
 
+/**相册库*/
+@property (nonatomic,strong) ALAssetsLibrary *library;
+
 @end
 
 @implementation XWSeeBigPictureViewController
+
+
+- (ALAssetsLibrary *)library
+{
+    if(!_library)
+    {
+        _library  = [[ALAssetsLibrary alloc]init];
+    }
+    return _library;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -37,7 +52,7 @@
     [scrollView addSubview: imageView];
     scrollView.delegate = self;
     self.imageView = imageView;
-
+    
     
     //设置图片位置
     imageView.x = 0;
@@ -76,8 +91,91 @@
 - (IBAction)back:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
-- (IBAction)save:(id)sender {
-    NSLogFunc;
+
+static NSString * const XWGroupNameKey = @"XWGroupNameKey";
+static NSString * const XWDefaultGroupName = @"百思不得姐";
+
+- (NSString *)groupName
+{
+    // 先从沙盒中取得名字
+    NSString *groupName = [[NSUserDefaults standardUserDefaults] stringForKey:XWGroupNameKey];
+    if (groupName == nil) { // 沙盒中没有存储任何文件夹的名字
+        groupName = XWDefaultGroupName;
+        
+        // 存储名字到沙盒中
+        [[NSUserDefaults standardUserDefaults] setObject:groupName forKey:XWGroupNameKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+    
+    return groupName;
 }
+
+- (IBAction)save:(id)sender {
+    
+    // 获得文件夹的名字
+    __block NSString *groupName = [self groupName];
+    
+    // self的弱引用
+    XWWeakSelf;
+    
+    __weak ALAssetsLibrary *weakLibrary = self.library;
+    
+    //创建一个新的文件夹
+    [weakLibrary addAssetsGroupAlbumWithName:groupName resultBlock:^(ALAssetsGroup *group) {
+        
+        if(group) //新建文件夹
+        {
+            [weakSelf addImageToGroup:group];
+        } else  //新建文件夹已经存在
+        {
+            //获得新建的文件夹
+            [weakLibrary enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                
+                NSString *name = [group valueForProperty:ALAssetsGroupPropertyName];
+            
+                if ([name isEqualToString:groupName]) { // 是自己创建的文件夹
+                    // 添加图片到文件夹中
+                    [weakSelf addImageToGroup:group];
+                    
+                    *stop = YES; // 停止遍历
+                } else if([name isEqualToString:@"Camera Roll"])
+                {
+                    // 文件夹被用户强制删除了
+                    groupName = [groupName stringByAppendingString:@" "];
+                    // 存储新的名字
+                    [[NSUserDefaults standardUserDefaults] setObject:groupName forKey:XWGroupNameKey];
+                    [[NSUserDefaults standardUserDefaults] synchronize];
+                    
+                    //新建文件夹，添加图片
+                    [weakLibrary addAssetsGroupAlbumWithName:groupName resultBlock:^(ALAssetsGroup *group) {
+                            [weakSelf addImageToGroup:group];
+                    } failureBlock:nil];
+                }
+                
+            } failureBlock:nil];
+        }
+    } failureBlock:nil];
+}
+
+/**
+ * 添加一张图片到某个文件夹中
+ */
+- (void) addImageToGroup:(ALAssetsGroup *)group
+{
+    __weak ALAssetsLibrary *weakLibrary = self.library;
+    
+    //获得保存的图片
+    CGImageRef image = self.imageView.image.CGImage;
+    
+    //1.先保存图片到系统相册
+    [weakLibrary writeImageToSavedPhotosAlbum:image metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
+        //2.在系统相册中找到图片,保存到自定义文件夹中
+        [weakLibrary assetForURL:assetURL resultBlock:^(ALAsset *asset) {
+            [group addAsset:asset];
+            [SVProgressHUD showSuccessWithStatus:@"保存成功!"];
+        } failureBlock:nil];
+    }];
+}
+
 
 @end
