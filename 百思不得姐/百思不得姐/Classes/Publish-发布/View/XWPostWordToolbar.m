35737e26174c565c196f730314f825f90a72c244
longxw1130 <longxw1130@126.com>
2015-10-05 02:13:41 +0800

    添加顶部工具栏..

--- 百思不得姐/百思不得姐/Classes/Publish-发布/View/XWPostWordToolbar.m
+++ 百思不得姐/百思不得姐/Classes/Publish-发布/View/XWPostWordToolbar.m
@@ -18,7 +18,26 @@
 
 - (void)awakeFromNib
 {
-    XWLog(@"%s",__func__);
+    //添加标签按钮
+    UIButton *addbutton = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    [addbutton setImage:[UIImage imageNamed:@"tag_add_icon"] forState:UIControlStateNormal];
+    
+    //自动计算尺寸
+    //sizeToFit生效，必须要先有内容
+    [addbutton sizeToFit];
+    
+    //监听事件
+    [addbutton addTarget:self action:@selector(addClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.topView addSubview:addbutton];
+}
+
+
+//添加标签
+- (void)addClick
+{
+    XWLog(@"点击了...");
 }
 
 @end
