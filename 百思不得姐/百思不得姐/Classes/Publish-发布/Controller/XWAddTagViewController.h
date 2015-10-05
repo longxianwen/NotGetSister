4c033dc3bcc52b7ab42a3605be1522bea1156ad4
longxw1130 <longxw1130@126.com>
2015-10-05 13:22:37 +0800

    添加标签控制器

---
+++ 百思不得姐/百思不得姐/Classes/Publish-发布/Controller/XWAddTagViewController.h
@@ -0,0 +1,13 @@
+//
+//  XWAddTagViewController.h
+//  百思不得姐
+//
+//  Created by longxianwen on 15/10/5.
+//  Copyright © 2015年 longxianwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XWAddTagViewController : UIViewController
+
+@end
---
+++ 百思不得姐/百思不得姐/Classes/Publish-发布/Controller/XWAddTagViewController.m
@@ -0,0 +1,37 @@
+//
+//  XWAddTagViewController.m
+//  百思不得姐
+//
+//  Created by longxianwen on 15/10/5.
+//  Copyright © 2015年 longxianwen. All rights reserved.
+//  添加标签
+
+#import "XWAddTagViewController.h"
+
+@interface XWAddTagViewController ()
+
+@end
+
+@implementation XWAddTagViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
